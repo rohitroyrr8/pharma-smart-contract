f4a954af6c603c0e4cf853c2245aafdc466e6f5d
rohit roy <rohitroyrr8@gmail.com>
2020-02-18 00:33:23 +0530

    drug func added.

--- src/services/DrugService.ts
+++ src/services/DrugService.ts
@@ -1,11 +1,19 @@
 import { PharmaNetContext } from "../contract/PharmanetContract";
 
 export class DrugService {
-    public async addDrug(ctx: PharmaNetContext, drugDto: string) {
+    public async addDrug(ctx: PharmaNetContext, drug: string) {
         return null;
     }
 
-    public async retailDrug(ctx: PharmaNetContext, drugDto: string) {
+    public async retailDrug(ctx: PharmaNetContext, drug: string) {
+        return null;
+    }
+
+    public async viewHistory(ctx: PharmaNetContext, drug: string) {
+        return null;
+    }
+
+    public async getDrugCurrentState(ctx: PharmaNetContext, drug: string) {
         return null;
     }
 }
